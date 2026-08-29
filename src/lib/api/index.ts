f43389e60af159e4c2e3c7a22b241b082a62b004
Saif Shahriar <sh.saif@proton.me>
2026-08-29 13:59:35 +0600

    feat: api barrel export

---
+++ src/lib/api/index.ts
@@ -0,0 +1,7 @@
+export { adminService } from "./admin";
+export { authService } from "./auth";
+export { courseService } from "./courses";
+export { enrollmentService } from "./enrollments";
+export { lessonService } from "./lessons";
+export { postService } from "./posts";
+export { quizService } from "./quizzes";
