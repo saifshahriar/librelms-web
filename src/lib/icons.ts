28c95b09e61fd5d784923e343441a710a5a738d4
Saif Shahriar <sh.saif@proton.me>
2026-08-29 16:27:58 +0600

    feat: fontawesome icon library

---
+++ src/lib/icons.ts
@@ -0,0 +1,33 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+	faArrowLeft,
+	faArrowRight,
+	faCheck,
+	faCircleExclamation,
+	faCircleQuestion,
+	faCompass,
+	faLock,
+	faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+	faArrowLeft,
+	faArrowRight,
+	faCheck,
+	faCircleExclamation,
+	faCircleQuestion,
+	faCompass,
+	faLock,
+	faXmark,
+);
+
+export {
+	faArrowLeft,
+	faArrowRight,
+	faCheck,
+	faCircleExclamation,
+	faCircleQuestion,
+	faCompass,
+	faLock,
+	faXmark,
+};
